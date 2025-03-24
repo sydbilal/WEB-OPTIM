e87ff17c1d6be1a90c993692bfa1c4223676a926
sydbilal <bilalnadeem09876@gmail.com>
2025-03-25 03:52:32 +0500

    deployement-changes

--- client/src/app/api/server/src/routes/seoRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { analyzeSEO } from "../controllers/seoController"; // Adjusted the import path
-
-const router = Router();
-
-router.post("/analyze", analyzeSEO); // Use the controller function
-
-export default router;
